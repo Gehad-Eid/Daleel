7decf240134429159e6f5862af3cd2da2b6e8bd2
gehad1995 <gehad.gehad1995@gmail.com>
2023-12-15 19:32:37 +0300

    Initial Commit

---
+++ Daleel/DaleelApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaleelApp.swift
+//  Daleel
+//
+//  Created by Gehad Eid on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DaleelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
